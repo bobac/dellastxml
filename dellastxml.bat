9d79b22b9042cf7e85013227d8445d8545b7fc28
Robert Houser <bobac@bobac.net>
2017-07-26 09:56:52 +0200

    initial commit

---
+++ dellastxml.bat
@@ -0,0 +1,4 @@
+del "c:\program files (x86)\advanced monitoring agent\LastPatchScan.xml"
+del "c:\program files (x86)\advanced monitoring agent (gp)\LastPatchScan.xml"
+echo LastPatchScan.xml deleted.
+cd .
